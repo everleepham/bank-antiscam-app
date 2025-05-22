ccb13aeba1a6cfe39e6cee224a924320c1d537a9
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-05-22 17:41:26 +0200

    update mongo model

--- app/models/mongo_model.py
+++ app/models/mongo_model.py
@@ -3,7 +3,6 @@ from typing import Optional
 from datetime import datetime
 from enum import Enum
 
-
 class TransactionStatus(str, Enum):
     VERIFIED = "verified"
     PENDING = "pending"
@@ -14,16 +13,26 @@ class User(BaseModel):
     fname: str = Field(..., description="User first name")
     lname: str = Field(..., description="User last name")
     email: EmailStr = Field(..., description="User email address")
+    password: str = Field(..., description="User password")
     new_user: bool = Field(default=True, description="Is the user new?")
     score: int = Field(default=0, description="User score")
 
 class Transaction(BaseModel):
     sender_id: str = Field(..., description="Sender user ID")
+    sender_device_id: str = Field(..., description="Unique device identifier")
     recipient_id: str = Field(..., description="Receiver user ID")
     amount: float = Field(..., gt=0, description="Transaction amount")
     timestamp: datetime = Field(default_factory=datetime.now, description="Transaction timestamp")
     status: TransactionStatus = Field(default=TransactionStatus.PENDING)
     flag_reason: Optional[str] = Field(default=None, description="Reason why transaction is flagged")
+    
+class DeviceLog(BaseModel):
+    device_id: str = Field(..., description="Unique device identifier")
+    user_id: str = Field(..., description="User ID")
+    mac_address: str = Field(..., description="Device MAC address")
+    ip_address: str = Field(..., description="Device IP address")
+    timestamp: datetime = Field(default_factory=datetime.now, description="Log timestamp")
+    location: str = Field(..., description="Device location")
         
         
 
