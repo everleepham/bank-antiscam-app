78c0b63b8cd2133170143ecf744c041d277c0e60
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-05-22 14:13:10 +0200

    add mongodb model

--- app/models/mongo_model.py
+++ app/models/mongo_model.py
@@ -1,29 +1,29 @@
-from pymongo import MongoClient
-from .base_model import BaseModel
-from app.utils.config import Config
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from datetime import datetime
+from enum import Enum
 
-class MongoModel(BaseModel):
-    def __init__(self):
-        self.client = MongoClient(Config.MONGO_URI)
-        self.db = self.client.get_database()
-        self.collection = self.db['items']
+
+class TransactionStatus(str, Enum):
+    VERIFIED = "verified"
+    PENDING = "pending"
+    SUSPICIOUS = "suspicious"
     
-    def create(self, data):
-        result = self.collection.insert_one(data)
-        return str(result.inserted_id)
-    
-    def read(self, item_id=None):
-        if item_id:
-            return self.collection.find_one({"_id": item_id})
-        return list(self.collection.find())
-    
-    def update(self, item_id, data):
-        result = self.collection.update_one(
-            {"_id": item_id},
-            {"$set": data}
-        )
-        return result.modified_count > 0
-    
-    def delete(self, item_id):
-        result = self.collection.delete_one({"_id": item_id})
-        return result.deleted_count > 0
+class User(BaseModel):
+    user_id: str = Field(..., description="Unique user identifier") # define user_id so we can use it with redis and neo4j
+    fname: str = Field(..., description="User first name")
+    lname: str = Field(..., description="User last name")
+    email: EmailStr = Field(..., description="User email address")
+    new_user: bool = Field(default=True, description="Is the user new?")
+    score: int = Field(default=0, description="User score")
+
+class Transaction(BaseModel):
+    sender_id: str = Field(..., description="Sender user ID")
+    recipient_id: str = Field(..., description="Receiver user ID")
+    amount: float = Field(..., gt=0, description="Transaction amount")
+    timestamp: datetime = Field(default_factory=datetime.now, description="Transaction timestamp")
+    status: TransactionStatus = Field(default=TransactionStatus.PENDING)
+    flag_reason: Optional[str] = Field(default=None, description="Reason why transaction is flagged")
+        
+        
+
